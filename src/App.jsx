cbec3c1b106281267a85f9394b9133ab4f8ad5a1
Mohamed Osama <141430040+mohammedd2510@users.noreply.github.com>
2025-12-23 21:09:09 +0200

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Hello from "./components/Hello";
-// const AWS_KEY = "kBCjngrI/tCH9OUiLx65jkXFqIJLcCLJfSwQBHx6";
+// const AWS_KEY = "";
 export default function App() {
   return (<div className="app"><h1>React Static Site</h1><Hello name="railway" /></div>);
 }
