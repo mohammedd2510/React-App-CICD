9a2e0292805a9125a835e8dd2279bae5e9f212c9
Mohamed Osama <141430040+mohammedd2510@users.noreply.github.com>
2026-02-11 09:31:50 +0200

    Use environment variable for Hello component name

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Hello from "./components/Hello";
+
 export default function App() {
-  return (<div className="app"><h1>React Static Site</h1><Hello name="railway" /></div>);
+  return (
+    <div className="app">
+      <h1>React Static Site</h1>
+      <Hello name={process.env.REACT_APP_NAME} />
+    </div>
+  );
 }
-
-// function App2() {
-//   console.log(undefinedVariable); // This will fail - variable not defined
-//   return <div>Test</div>;
-// }
-
